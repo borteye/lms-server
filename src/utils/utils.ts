fb902629532752d49fa5c0de3a409bdab254bd3c
borteye <gabrielborteye12199@gmail.com>
2025-09-01 10:52:17 +0000

    feat: add token generation and file type validation utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,7 +4,7 @@ import { ApiResponse } from "../types/shared";
 
 export const generateAccessToken = (jwtPayload: any) => {
   return jwt.sign(jwtPayload, process.env.ACCESS_TOKEN_SECRET_KEY as Secret, {
-    expiresIn: "2d",
+    expiresIn: "1d",
   });
 };
 
@@ -13,6 +13,14 @@ export const generatePasswordResetToken = (jwtPayload: any) => {
     jwtPayload,
     process.env.PASSWORD_RESET_TOKEN_SECRET_KEY as Secret,
     { expiresIn: "30m" }
+  );
+};
+
+export const generateEmailVerificationToken = (jwtPayload: any) => {
+  return jwt.sign(
+    jwtPayload,
+    process.env.EMAIL_VERIFICATION_TOKEN_SECRET_KEY as Secret,
+    { expiresIn: "1h" }
   );
 };
 
@@ -37,4 +45,49 @@ export function ResponseStructure<T>(props: ApiResponse<T>): ApiResponse<T> {
     subCode: props.subCode,
     errors: props.errors ?? null,
   };
+}
+
+export const imageMimeTypes = [
+  "image/jpeg",
+  "image/png",
+  "image/webp",
+  "image/jpg",
+  "image/svg+xml",
+  "image/bmp",
+  "image/tiff",
+];
+
+export const excelMimeTypes = [
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", // .xlsx
+  "application/vnd.ms-excel", // .xls
+  "text/csv", // .csv (often used with Excel data)
+  "application/vnd.ms-excel.sheet.macroEnabled.12", // .xlsm
+  "application/vnd.ms-excel.template.macroEnabled.12", // .xltm
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.template", // .xltx
+];
+
+export const isImageFile = (mimetype: string): boolean => {
+  return imageMimeTypes.includes(mimetype);
+};
+
+export const isExcelFile = (mimetype: string): boolean => {
+  return excelMimeTypes.includes(mimetype);
+};
+
+export function generateUUID(): string {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0; // random number 0-15
+    const v = c === "x" ? r : (r & 0x3) | 0x8; // for "y" keep it in 8–b range
+    return v.toString(16);
+  });
+}
+
+export function generatePassword(): string {
+  const chars =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let result = "";
+  for (let i = 0; i < 8; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
 }
